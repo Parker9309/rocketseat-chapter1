994104b56164283a0555c9bce89febbafd88f854
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-01-07 18:36:36 -0300

    feat: RepositoryList created

---
+++ chapter1/src/Components/repositoryList.jsx
@@ -0,0 +1,33 @@
+function RepositoryList() {
+  return (
+    <section className="repositoryList">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        <li className="lists">
+          <strong>unform</strong>
+          <p>form in react</p>
+          <a href="">Acessar repositório</a>
+
+        </li> <li className="lists">
+          <strong>unform</strong>
+          <p>form in react</p>
+          <a href="">Acessar repositório</a>
+          
+        </li> <li className="lists">
+          <strong>unform</strong>
+          <p>form in react</p>
+          <a href="">Acessar repositório</a>
+
+        </li> <li className="lists">
+          <strong>unform</strong>
+          <p>form in react</p>
+          <a href="">Acessar repositório</a>
+        </li>
+      </ul>
+
+    </section>
+  )
+}
+
+export default RepositoryList
