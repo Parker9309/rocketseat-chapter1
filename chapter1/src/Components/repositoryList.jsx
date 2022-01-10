41dd8e1a148168453f89d1608d8aea63ad7f9e64
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-01-10 12:18:03 -0300

    feat: repositoryList changes

--- chapter1/src/Components/repositoryList.jsx
+++ chapter1/src/Components/repositoryList.jsx
@@ -1,29 +1,21 @@
+import RepositoryItem from "./repositoryItem"
+
+const repository = {
+  name: 'unform',
+  description: 'From in react',
+  link: 'https://github.com/unform/unform'
+}
+
 function RepositoryList() {
   return (
     <section className="repositoryList">
       <h1>Lista de Repositórios</h1>
 
       <ul>
-        <li className="lists">
-          <strong>unform</strong>
-          <p>form in react</p>
-          <a href="">Acessar repositório</a>
-
-        </li> <li className="lists">
-          <strong>unform</strong>
-          <p>form in react</p>
-          <a href="">Acessar repositório</a>
-          
-        </li> <li className="lists">
-          <strong>unform</strong>
-          <p>form in react</p>
-          <a href="">Acessar repositório</a>
-
-        </li> <li className="lists">
-          <strong>unform</strong>
-          <p>form in react</p>
-          <a href="">Acessar repositório</a>
-        </li>
+       <RepositoryItem repository={ repository }/>
+       <RepositoryItem />
+       <RepositoryItem />
+       <RepositoryItem />
       </ul>
 
     </section>
